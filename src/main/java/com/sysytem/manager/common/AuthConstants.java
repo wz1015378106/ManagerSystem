0cd5a39be3354a5d75d3871d54712c99432ad409
wangzhi <wangzhi@idstaff.com>
2021-07-23 17:40:05 +0800

    测试提交

---
+++ src/main/java/com/sysytem/manager/common/AuthConstants.java
@@ -0,0 +1,13 @@
+package com.sysytem.manager.common;
+
+/**
+ * @author: wangzhi
+ * @Title: AuthConstants
+ * @Description:
+ * @date: 2021/7/23 17:37
+ */
+public class AuthConstants {
+    public final static String USERID = "userId";
+    public final static String ROLE = "role";
+    public final static String USERNAME = "userName";//商家账号名
+}
